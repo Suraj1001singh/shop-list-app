f5e4683c9e72dd90f050f0ef5e329a3d50f62abe
Suraj Singh <absurajsinghchamiyal@gmail,com>
2022-06-13 14:13:16 +0530

    inserting secrete keys

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -14,3 +14,15 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+
+//secrete keys
+
+// REACT_APP_API_KEY=AIzaSyCCbYxiXTxRkiZehoFLY6CTZlOJ13_hMRA
+// REACT_APP_AUTH_DOMAIN=shop-list-app-5406c.firebaseapp.com
+// REACT_APP_DB_URL=https://shop-list-app-5406c-default-rtdb.firebaseio.com
+// REACT_APP_PROJECT_ID=shop-list-app-5406c
+// REACT_APP_STORAGE_BUCKET=shop-list-app-5406c.appspot.com
+// REACT_APP_MSG_ID=183298806749
+// REACT_APP_APP_ID=1:183298806749:web:1d0332610a750604b1dc34
+// REACT_APP_MEASURE_ID=G-CCHBE9J9ES
